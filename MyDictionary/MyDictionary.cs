c2c4bf269a99da9e4275c864e012ae6dea858714
Gokhan <gokhannn3855@gmail.com>
2021-03-01 20:51:36 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    public class MyDictionary<K, V>
+    {
+        private K[] keys;
+        private V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+        public void Add(K key, V value)
+        {
+            K[] tempKey = keys;
+            V[] tempValue = values;
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+                values[i] = tempValue[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public void ToList()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("[" + keys[i] + "," + values[i] + "]");
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> Ogrenci = new MyDictionary<int, string>();
+            Ogrenci.Add(1, "Gökhan KARAKUŞ");
+            Ogrenci.Add(2, "X Y");
+            Ogrenci.Add(3, "A B");
+            Ogrenci.Add(4, "W Z");
+
+            Ogrenci.ToList();
+
+        }
+    }
+}
